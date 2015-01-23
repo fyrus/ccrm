ed66dc75b1e59e41f8bd9c2aeb3997202e9b4bc8
Nastia <Nastia@9>
2015-01-23 20:10:49 +0200

    h

--- Prototype3/src/gui/NewSalesManEvent.java
+++ Prototype3/src/gui/NewSalesManEvent.java
@@ -13,8 +13,20 @@ import java.awt.Font;
 
 import javax.swing.JTextField;
 
+import client.ChatClient;
+
 import com.toedter.calendar.JDateChooser;
 
+import common.ChatIF;
+import common.Com;
+import common.Command;
+import entities.Domain;
+import entities.Permission;
+import entities.Product;
+import entities.RegisteredCustomer;
+import entities.Sale;
+
+import javax.swing.JComboBox;
 import javax.swing.JOptionPane;
 import javax.swing.JRadioButton;
 import javax.swing.JRadioButtonMenuItem;
@@ -23,30 +35,44 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JButton;
 import javax.swing.border.MatteBorder;
+import javax.swing.event.AncestorEvent;
+import javax.swing.event.AncestorListener;
 
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * @author Nastia
  *
  */
-public class NewSalesManEvent extends JPanel {
+public class NewSalesManEvent extends JPanel implements ChatIF{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JTextField tfpID;
-	private JTextField tfcstID;
 	public JButton btnCancel;
 	public JButton btnPurchase;
 	public JButton btnRejection;
+	
+	private ChatClient client;
+	private JComboBox<RegisteredCustomer> cbCustomerId;
+	private JComboBox<Product> cbProductName;
+	
+	private int productLen;
+	private ArrayList<Product> productList;
+	private int registeredCustomerLen;
+	private ArrayList<RegisteredCustomer> registeredCustomerList;
 
 	/**
 	 * Create the panel.
 	 */
 	public NewSalesManEvent() {
+		
+		LoadData();
+		
 		setSize(new Dimension(700, 380));
 		setBackground(Color.GRAY);
 		setLayout(null);
@@ -75,18 +101,37 @@ public class NewSalesManEvent extends JPanel {
 		lblDate.setBounds(54, 170, 94, 16);
 		add(lblDate);
 		
-	
-		
-		tfpID = new JTextField();
-		tfpID.setBounds(189, 109, 116, 22);
-		add(tfpID);
-		tfpID.setColumns(10);
-		
-		final JDateChooser tfdate = new JDateChooser();
-		tfdate.setBounds(189, 164, 116, 22);
-		add(tfdate);
+		final JDateChooser dcDate = new JDateChooser();
+		dcDate.setBounds(189, 164, 116, 22);
+		add(dcDate);
 		
 		btnPurchase = new JButton("Purchase");
+		btnPurchase.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				Command cmd = new Command();
+				Sale sale = new Sale();
+
+				for(int i=0;i<productLen;i++)
+					if (productList.get(i).getPname().equals(cbProductName.getSelectedItem())) 
+						sale.setItemid(productList.get(i).getPid());
+
+				for(int i=0;i<registeredCustomerLen;i++)
+					if (registeredCustomerList.get(i).getcId().equals(cbCustomerId.getSelectedItem())) 
+						sale.setCustomerid(registeredCustomerList.get(i).getcId());
+				sale.setSaleDate((java.sql.Date)(dcDate.getDate()));
+				
+
+				cmd.setComVal(sale);
+				cmd.setComNum(Com.ADD_SALE);
+
+				client.handleMessageFromClientUI(cmd);
+				
+				JOptionPane.showConfirmDialog(null, "Sale was added successfully");
+				dcDate.setDate(null);
+				LoadData();
+			}
+		});
 		btnPurchase.setBorder(new MatteBorder(2, 2, 2, 2, (Color) Color.PINK));
 		btnPurchase.setBackground(new Color(230, 230, 250));
 		btnPurchase.setBounds(452, 161, 188, 36);
@@ -111,18 +156,12 @@ public class NewSalesManEvent extends JPanel {
 		btnRejection.setBounds(452, 51, 188, 36);
 		add(btnRejection);
 		
-		tfcstID = new JTextField();
-		tfcstID.setBounds(189, 58, 116, 22);
-		add(tfcstID);
-		tfcstID.setColumns(10);
-		
 		btnCancel = new JButton("Cancel");
 		btnCancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				lblRejectionField.setText("");
-				tfdate.setDate(null);
-				tfcstID.setText("");
-				tfpID.setText("");
+				dcDate.setDate(null);
+				
 				
 			}
 		});
@@ -131,7 +170,92 @@ public class NewSalesManEvent extends JPanel {
 		btnCancel.setBounds(452, 293, 188, 36);
 		add(btnCancel);
 		
+		cbCustomerId = new JComboBox<RegisteredCustomer>();
+		cbCustomerId.setBounds(189, 58, 116, 22);
+		add(cbCustomerId);
+		
+		cbProductName = new JComboBox<Product>();
+		cbProductName.setBounds(189, 109, 116, 22);
+		add(cbProductName);
+		
+		connect();
 		
 
+	}
+
+	
+	private void LoadData(){
+		addAncestorListener(new AncestorListener() {
+			public void ancestorAdded(AncestorEvent arg0) {
+				
+				cbCustomerId.removeAllItems();
+				cbProductName.removeAllItems();
+				
+				Command cmd = new Command();
+				
+				Sale sale = new Sale();
+				cmd.setComVal(sale);
+				cmd.setComNum(Com.SEARCH_SALE);
+				client.handleMessageFromClientUI(cmd);
+				
+				Product product = new Product();
+				cmd.setComVal(product);
+				cmd.setComNum(Com.SEARCH_PRODUCT);
+				client.handleMessageFromClientUI(cmd);
+
+				RegisteredCustomer registeredCustomer = new RegisteredCustomer();
+				cmd.setComVal(registeredCustomer);
+				cmd.setComNum(Com.SEARCH_REGISTEREDCUSTOMER);
+				client.handleMessageFromClientUI(cmd);
+				
+				
+			}
+			public void ancestorMoved(AncestorEvent arg0) {
+			}
+			public void ancestorRemoved(AncestorEvent arg0) {
+			}
+		});
+	}
+
+
+
+
+	// make a connection to server
+	private void connect(){
+
+		try 
+		{
+			client= new ChatClient(Login.IP,Login.D_PORT,this);
+		} 
+		catch(IOException exception) 
+		{
+			System.out.println("Error: Can't setup connection!"
+					+ " Terminating client.");
+			System.exit(1);
+		}
+
+	}
+
+	/* (non-Javadoc)
+	 * @see common.ChatIF#display(java.lang.Object)
+	 */
+	@Override
+	public void display(Object message) {
+		// TODO Auto-generated method stub
+		if (message instanceof ArrayList<?>) {
+			for(Object key:((ArrayList<?>)message).toArray()){
+				if(key instanceof Product){
+					cbProductName.addItem(((Product)key));
+				}
+				if(key instanceof RegisteredCustomer){
+					cbCustomerId.addItem(((RegisteredCustomer)key));
+					
+					
+				}
+
+			}
+
+		}
+		
 	}
 }
